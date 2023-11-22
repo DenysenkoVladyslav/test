53be454a2cbd62e58e3ec32f5b1980028edeb0d6
vlad <denysenko.vladyslav1123@vu.cdu.edu.ua>
2023-11-22 19:39:53 +0200

    Зафіксовано зміни

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿ыusing System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
